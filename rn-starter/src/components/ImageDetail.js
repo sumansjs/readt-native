5f76ca23c982263980d4347072172ad8c60a9781
sumansjs <suman.hansada@gmail.com>
2020-05-31 16:29:49 +0530

    Added parent child relationship

---
+++ rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const ImageDetail = props => {
+return <Text>{props.title}</Text>
+}
+
+const style = StyleSheet.create({});
+export default ImageDetail
---
+++ rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
+
+const ImageScreen = () => {
+  return(
+    <View>
+    <ImageDetail title='Forest' />
+    <ImageDetail title='Beach' />
+    <ImageDetail title='Mountain' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
