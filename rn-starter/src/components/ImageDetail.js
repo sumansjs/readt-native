17ac003307a68a96aa5f11bbc20a750173d7f008
sumansjs <suman.hansada@gmail.com>
2020-05-31 16:50:04 +0530

    Added images and image source in components

--- rn-starter/src/components/ImageDetail.js
+++ rn-starter/src/components/ImageDetail.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = props => {
-return <Text>{props.title}</Text>
+const ImageDetail = ({ imageSource, title, score }) => {
+  return (
+  <View>
+    <Image source={imageSource} />
+    <Text>{title}</Text>
+    <Text>Image Score - {score}</Text>
+  </View>
+  );
 }
 
 const style = StyleSheet.create({});
--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -5,9 +5,21 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
   return(
     <View>
-    <ImageDetail title='Forest' />
-    <ImageDetail title='Beach' />
-    <ImageDetail title='Mountain' />
+    <ImageDetail
+      title='Forest'
+      score={9}
+      imageSource={require('../../assets/forest.jpg')}
+    />
+    <ImageDetail
+      title='Beach'
+      score={7}
+      imageSource={require('../../assets/beach.jpg')}
+    />
+    <ImageDetail
+      title='Mountain'
+      score={4}
+      imageSource={require('../../assets/mountain.jpg')}
+    />
     </View>
   );
 }
