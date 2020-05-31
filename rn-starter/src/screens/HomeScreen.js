d01bf7bea8445c1f61225d88f15b53f13d89bd40
sumansjs <suman.hansada@gmail.com>
2020-05-31 13:46:02 +0530

    Added TouchableOpacity

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+// For Buttons, we have two react-native components
+// Button -> For simple button component with onPress and title props
+// TouchableOpacity -> Highly customizable component to detect press on any kind of element
+// For Events, we have onPress property
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+  <View>
+    <Text style={styles.text}>HomeScreen</Text>
+    <Button
+      onPress={() => console.log('Button pressed')}
+      title="Go to Components Demo"
+    />
+    <TouchableOpacity onPress={() => console.log('List Pressed')}>
+      <Text>Go to List Demo</Text>
+      <Text>Go to List Demo</Text>
+      <Text>Go to List Demo</Text>
+    </TouchableOpacity>
+  </View>
+  );
 };
 
 const styles = StyleSheet.create({
