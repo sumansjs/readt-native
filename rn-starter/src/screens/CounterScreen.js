a504d3cc3291c08c5f6fbb6a749c181314e1bcee
sumansjs <suman.hansada@gmail.com>
2020-06-11 07:58:34 +0530

    Added hooks to set and change the state

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-let counter = 0;
+// useState is a hook
+// A hook is a function which adds new functionality to function component
 
 const CounterScreen = () => {
+  // Array destructuring syntax here -
+  // useState(0) - setting the initial state of counter to - 0
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          counter++;
-          console.log(counter);
+          // setting the new state of counter
+          setCounter(counter + 1);
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          counter--;
-          console.log(counter);
+          // setting the new state of counter
+          setCounter(counter - 1);
         }}
         />
-      <Text>Current Count:</Text>
+      <Text>Current Count: { counter }</Text>
     </View>
   );
 }
