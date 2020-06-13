c98569d924f2fe387d12fc7f7ae48b96ad6ce044
sumansjs <suman.hansada@gmail.com>
2020-06-13 20:10:02 +0530

    Added useReducer in place of useState

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,53 +1,77 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+// Reducer -
+// Function that manages changes to an object
+// It gets called with two object'
+// 1st argument - Object that has all of out state in it
+// 2nd argument - Object that describes the update we want to make
+// Notes -
+// 1. Never change 1st argument directly
+// 2. Always return a value to be used as 1st argument
+
+// When to use a reducer ?
+// - When you have related pieces of state (Here it is - 3 colors)
+// - When we know precisely no of ways in which we can update state (Here it is - 6)
+
+const reducer = (state, action) => {
+  // state === { red: number, green: number, blue: number }
+  // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+  switch (action.colorToChange) {
+    case 'red':
+      // Never do like this -
+      // state.red = state.red - 15
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
+    case 'green':
+      return state.green + action.amount > 255 || state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
 
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state;
+  console.log(state);
 
-  const setColor = (color, change) => {
-    // color === 'red', 'green', 'blue'
-    // change === +15, -15
-    switch (color) {
-      case 'red':
-        (color + change > 255 || color + change < 0) ? null : setRed(red + change);
-        break;
-      case 'green':
-        (color + change > 255 || color + change < 0) ? null : setGreen(green + change);
-        break;
-      case 'blue':
-        (color + change > 255 || color + change < 0) ? null : setBlue(blue + change);
-        break;
-      default:
-        break;
-    }
-  }
-
-  return(
+  return (
     <View>
       <ColorCounter
         color="red"
-        onIncrease={() => setColor('red', COLOR_INCREMENT)}
-        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} />
+        onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
+      />
       <ColorCounter
         color="green"
-        onIncrease={() => setColor('green', COLOR_INCREMENT)}
-        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}/>
+        onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
+      />
       <ColorCounter
         color="blue"
-        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
-        onDecrease={() => setColor('blue', - 1 * COLOR_INCREMENT)}/>
+        onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
+      />
       <View
-        style={{height: 100, width: 100, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}>
-      </View>
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
+      ></View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({});
 export default SquareScreen;
