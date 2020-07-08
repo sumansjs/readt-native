e7478bd5880ebdf1fc42cc2d086622ad7ed2abb9
sumansjs <suman.hansada@gmail.com>
2020-07-08 07:54:16 +0530

    Added useEffect hook

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import zomato from '../api/zomato';
 
 const SearchScreen = () => {
+  // useEffect is a hook or function that allows to run some piece of code just one time
+  // when our component is first rendered to screen
+
+  // useEffect's Second Argument
+  // useEffect(() => {})           -> Run the arrow function every time the component is rendered
+  // useEffect(() => {}, [])       -> Run the arrow function only when the component is first rendered
+  // useEffect(() => {}, [value])  -> Run the arrow function only when the component is first rendered
+  //                                 and when the value changes
+
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there');
     try {
       const response = await zomato.get('/search', {
         params: {
           entity_id: 5,
           entity_type: 'city',
-          q: term,
+          q: searchTerm,
         },
       });
       setResults(response.data.restaurants);
@@ -23,9 +33,17 @@ const SearchScreen = () => {
     }
   };
 
+  // call searchApi when component is first rendered
+  // BAD CODE!
+  // searchApi('pasta');
+  // RIGHT WAY!
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
