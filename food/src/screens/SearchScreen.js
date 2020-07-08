a56f951b586f391a5af7eebb4dd2f0b4782babd2
sumansjs <suman.hansada@gmail.com>
2020-07-08 07:16:59 +0530

    Added axios and search functionality

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import zomato from '../api/zomato';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await zomato.get('/search', {
+        params: {
+          entity_id: 5,
+          entity_type: 'city',
+          q: term,
+        },
+      });
+      setResults(response.data.restaurants);
+    } catch (error) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
-      />
-      <Text>SearchScreen</Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
