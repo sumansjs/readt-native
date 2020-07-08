2c0e33933c9893157298a343a22b13791a212341
sumansjs <suman.hansada@gmail.com>
2020-07-08 09:11:58 +0530

    Extracted hook logic to useResults hook

---
+++ food/src/hooks/useResults.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import zomato from '../api/zomato';
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there');
+    try {
+      const response = await zomato.get('/search', {
+        params: {
+          entity_id: 5,
+          entity_type: 'city',
+          q: searchTerm,
+        },
+      });
+      setResults(response.data.restaurants);
+    } catch (error) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  // useEffect is a hook or function that allows to run some piece of code just one time
+  // when our component is first rendered to screen
+
+  // useEffect's Second Argument
+  // useEffect(() => {})           -> Run the arrow function every time the component is rendered
+  // useEffect(() => {}, [])       -> Run the arrow function only when the component is first rendered
+  // useEffect(() => {}, [value])  -> Run the arrow function only when the component is first rendered
+  //                                 and when the value changes
+
+  // call searchApi when component is first rendered
+  // BAD CODE!
+  // searchApi('pasta');
+  // RIGHT WAY!
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
+  return [results, errorMessage, searchApi];
+};
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,45 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import zomato from '../api/zomato';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
-  // useEffect is a hook or function that allows to run some piece of code just one time
-  // when our component is first rendered to screen
-
-  // useEffect's Second Argument
-  // useEffect(() => {})           -> Run the arrow function every time the component is rendered
-  // useEffect(() => {}, [])       -> Run the arrow function only when the component is first rendered
-  // useEffect(() => {}, [value])  -> Run the arrow function only when the component is first rendered
-  //                                 and when the value changes
-
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    console.log('Hi there');
-    try {
-      const response = await zomato.get('/search', {
-        params: {
-          entity_id: 5,
-          entity_type: 'city',
-          q: searchTerm,
-        },
-      });
-      setResults(response.data.restaurants);
-    } catch (error) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  // call searchApi when component is first rendered
-  // BAD CODE!
-  // searchApi('pasta');
-  // RIGHT WAY!
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
+  const [results, errorMessage, searchApi] = useResults();
 
   return (
     <View>
