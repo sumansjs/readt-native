390bdd5c4a541015107c881404c0d9634ce19bb2
sumansjs <suman.hansada@gmail.com>
2020-07-12 19:52:55 +0530

    Added Flatlist rendering

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, FlatList } from 'react-native';
 
 const ResultsList = ({ results, title }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={(result) => result.restaurant.id}
+        renderItem={({ item }) => {
+          return <Text>{item.restaurant.name}</Text>;
+        }}
+      />
     </View>
   );
 };
