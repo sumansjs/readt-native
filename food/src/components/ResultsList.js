7ce622cb3a1ac9df454f51bc3c5f3c390722922c
sumansjs <suman.hansada@gmail.com>
2020-07-14 22:36:28 +0530

    Added ScrollView, Removed Scrollbars

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -8,6 +8,7 @@ const ResultsList = ({ results, title }) => {
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.restaurant.id}
         renderItem={({ item }) => {
