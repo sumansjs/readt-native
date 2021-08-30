e991dd292ad926c82086f57fb3a49c961d44edd5
Suman Hansada <suman.hansada@gmail.com>
2021-08-30 17:57:12 +0530

    Styling the IndexScreen

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   /*
@@ -16,12 +17,34 @@ const IndexScreen = () => {
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash" />
+            </View>
+          );
+        }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
